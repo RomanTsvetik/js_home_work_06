647a2453d73909a2d3e5d91dfa53d4b83a61dc70
RomanTsvetik <zvetik85@gmail.com>
2021-06-17 13:20:38 +0300

    commit 1

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+//Task #1
+
+Boolean(Number('10')) + 1; // - вернет 2 (тип Number)
+'sub' + Number(false); // вернет 'sub0' (тип String) - конкатенация 
+16 * "91"; // вернет 1456 (тип Number)
+true - 70; // вернет -69 (тип Number)
+Number(1 + String(1)) + 1; // вернет 12 (тип Number)
+
+
+
+
+
---
+++ js/secInHour.js
@@ -0,0 +1,8 @@
+//Task #2 
+
+let secInHour;
+
+let hour = prompt('pls enter number of hours', '');
+
+secInHour = hour * 3600;
+alert(`there are ${secInHour} seconds in ${hour} hours`);
